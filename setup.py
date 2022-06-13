a27c6615aa24827119b21f55f1e8d187c0418f19
alessandro <alessandro.gianfelici@hotmail.com>
2022-06-13 20:44:10 +0200

    initial version

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='hycom_downloader',
+      version='0.1.0',
+      description='Script to download simulation data from HYCOM.org',
+      url='https://github.com/AlessandroGianfelici/hycom_downloader',
+      author='Alessandro Gianfelici',
+      author_email='alessandro.gianfelici@hotmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'pandas', 
+          'beautifulsoup4',
+          'requests',
+          'xarray',
+          'toolz'
+      ],
+      zip_safe=False)
