3142fcbd4477f0a44891338b0f49ebbc8ca9e148
alessandro <alessandro.gianfelici@hotmail.com>
2022-06-13 21:29:08 +0200

    set parallel

--- hycom_downloader/__init__.py
+++ hycom_downloader/__init__.py
@@ -52,7 +52,7 @@ def download(data_set_url, lat, lon, requested_cols=None):
 
 def download_all(url_list, lat, lon, requested_cols=None):
     download_lat_lon = partial(download, lat=lat, lon=lon, requested_cols=requested_cols)
-    pool = Pool(7)
+    pool = Pool(multiprocessing.cpu_count()-1)
     list_data = pool.map(download_lat_lon, url_list)
     pool.close()                           
     return pd.concat(list_data).reset_index()
